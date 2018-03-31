423c9c406b19f83aec13ab94c3a7109edca0331e
philip.zhou <philip.zhou.2009@gmail.com>
2018-03-31 19:14:19 +0800

    commit spring starter project

---
+++ src/main/java/com/pmcb/sbexp/SbexpApplication.java
@@ -0,0 +1,12 @@
+package com.pmcb.sbexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbexpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pmcb/sbexp/SbexpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pmcb.sbexp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbexpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
