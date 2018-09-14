f54b706d22d14ea391f9f9e648f0125469a15edc
Philip Zhou <philip_zhou@singaporeair.com.sg>
2018-09-14 21:45:52 +0800

    remove obsolete file to make build pass

--- src/main/java/com/pmcb/sbexp/GreetingController.java
+++ src/main/java/com/pmcb/sbexp/GreetingController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-
-
     @GetMapping(path = "/greeting")
     public String greeting() {
 
